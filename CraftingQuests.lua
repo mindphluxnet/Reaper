c2637bf334c55cfb844e7886d5dad5573c76e6d6
Richard Kämmerer <richard.kaemmerer@gmail.com>
2023-06-14 23:56:24 +0200

    More crafting quests

--- CraftingQuests.lua
+++ CraftingQuests.lua
@@ -1,7 +1,11 @@
 CraftingQuests={
     [1072] = "Alchemy", -- An Old Colleague, followup quest requires two items made by Alchemy of which only one can be found as a common drop
     [713] = "Alchemy", -- Coolant Heads Prevail, requires Frost Oil made by Alchemy
-    [565] = "Leatherworking", -- Bartolo's Yeti Fur Cloak, requires Hillman's Cloak made by Leatherworking (and Bolt of Wool Cloth but that can be commonly found in chests)
+    [565] = "Leatherworking", -- Bartolo's Yeti Fur Cloak, requires Hillman's Cloak made by Leatherworking
     [4243] = "Engineering", -- Chasing A-ME01, requires Mithril Casing made by Engineering
     [716] = "Blacksmithing", -- Stone Is Better Than Cloth, requires Patterned Bronze Bracers made by Blacksmithing
+    [4736] = "Tailoring", -- In Search of Menara Voidrender, next step requires Robes of Arcana made by Tailoring
+    [4737] = "Tailoring", -- same quest for Horde
+    [210] = "Mining", -- Krazek's Cookery, followup quest requires Mining
+    [343] = "Mining", -- Speaking of Fortitude, chain includes a quest requiring Mining
 }
